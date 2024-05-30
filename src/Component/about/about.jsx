c7c5f19f7e8b866a9c62d755e6cdb1ab0c6bde2d
Kartikay786 <kg5724443@gmail.com>
2024-05-30 16:39:06 +0530

    .

--- src/Component/about/about.jsx
+++ src/Component/about/about.jsx
@@ -18,7 +18,7 @@ function About(){
                     <div className="aboutright">
                         <div className="text">
                             <div className="textbox">
-                                <h2>I'm  Kumar Kartikay</h2>
+                                <h2>I'm Kartikay</h2>
                                 <p>Self-motivated and hardworking fresher seeking for an opportunity to work in a challenging environment to prove my skills and utilize my knowledge & intelligence in the growth of the organization.</p>
                                 <button>Download CV</button>
                             </div>
